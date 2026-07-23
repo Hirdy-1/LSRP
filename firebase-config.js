0638d9c1f68537c7bcab972a95fb0e560d592f26
Hirdy-1 <dominichird4@gmail.com>
2026-07-23 22:50:47 +0100

    Update Firebase configuration by removing measurementId
    Removed measurementId from Firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyA2vOAHp5AmUTGpjTzR4_aH3AsNQhjooxk",
     authDomain: "lsrp-a5106.firebaseapp.com",
@@ -14,7 +10,6 @@
     storageBucket: "lsrp-a5106.firebasestorage.app",
     messagingSenderId: "102025009574",
     appId: "1:102025009574:web:ff4e54f33ab23ad2775767",
-    measurementId: "G-4Q9KYFNETX"
   };
 
   // Initialize Firebase
